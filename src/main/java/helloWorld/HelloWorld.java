5ae7bdbff8e71df39c752b3a423d440516a118ea
Henric Wiktor <155428345+phumpeti@users.noreply.github.com>
2026-03-30 12:55:19 +0200

    Add breakfast suggestion to HelloWorld output

--- src/main/java/helloWorld/HelloWorld.java
+++ src/main/java/helloWorld/HelloWorld.java
@@ -3,5 +3,6 @@ package helloWorld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World! What do you want for breakfast?");
+        System.out.println("Green eggs and ham");
     }
 }
