1904bca4eb9e0aba74dcafa80ab41b65477128bf
akari <a.karin.larsson@gmail.com>
2026-03-30 11:17:57 +0200

    ??

--- src/main/java/helloWorld/HelloWorld.java
+++ src/main/java/helloWorld/HelloWorld.java
@@ -3,6 +3,6 @@ package helloWorld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World! What do you want for dinner?");
-        System.out.println("Are you even hungry?");
+        System.out.println("Are you even hungry??");
     }
 }
